a4392586a7cace02f76a0e79e84a966ed5585739
jonathan sahar <jonathan.sahar.com>
2024-06-11 11:14:06 +0300

    update figures dir (auto)

--- plotTimeCourseMotorSubplots.m
+++ plotTimeCourseMotorSubplots.m
@@ -64,7 +64,7 @@ for i = 1:length(cortices)
     end
 
     % Adding common title
-    title(tcl, 'Mean BOLD signal\nSilent (motor-only) condition', 'FontSize', fontsize);
+    title(tcl, 'nSilent (motor-only) condition', 'FontSize', fontsize);
 
     % Saving the figure to a jpg file
     titleStr = sprintf("Average Activity Over Time Motor Cortex");
